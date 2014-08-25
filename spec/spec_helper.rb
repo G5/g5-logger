5df61c5afb39bf8862003d6169b0527cf5357e2a
Perry Hertler <perry@hertler.org>
2014-08-25 08:37:12 -0500

    configuring vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,8 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered                = true
   config.filter_run :focus
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+end
